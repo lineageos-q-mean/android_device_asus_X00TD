ba1455a12c5a71986c3e5551e888f9e6c8c9a2aa
Kneba <abenkenary3@gmail.com>
2025-05-12 09:01:24 +0000

    X00TD: Clone our stuff from vendorsetup
    Change-Id: I36bfaad8aac1248aa0bb96ef209caf4feff3cb25

---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export TZ=Asia/Jakarta
+
+rm -rf kernel/asus/sdm660
+git clone --depth=1 --recursive https://github.com/Kneba/kernel_asus_sdm660 -b clo-ksun kernel/asus/sdm660
+
+rm -rf vendor/lineage-priv/keys
+mkdir -p vendor/lineage-priv/keys
+git clone https://github.com/electrolaboratory/public-keys vendor/lineage-priv/keys/
