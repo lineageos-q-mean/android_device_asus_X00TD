ad1f21129709f621f90c3752d5d9c26a4d37207e
Kneba <abenkenary3@gmail.com>
2025-05-12 17:38:17 +0700

    X00TD: Force Using KSU
    FIXED: 
    build/make/core/base_rules.mk:324: error: system/extras/su: MODULE.TARGET.EXECUTABLES.su already defined by kernel/asus/sdm660/KernelSU-Next/userspace/su/jni.
    16:34:55 ckati failed with: exit status 1

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,9 @@ export TZ=Asia/Jakarta
 rm -rf kernel/asus/sdm660
 git clone --depth=1 --recursive https://github.com/Kneba/kernel_asus_sdm660 -b clo-ksun kernel/asus/sdm660
 
+# Force Using KSU
+rm -rf system/extras/su
+
 rm -rf vendor/lineage-priv/keys
 mkdir -p vendor/lineage-priv/keys
 git clone https://github.com/electrolaboratory/public-keys vendor/lineage-priv/keys/
