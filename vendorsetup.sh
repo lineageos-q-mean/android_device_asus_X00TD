943988a0a845733be9e337d772d4c86c8fa28529
texascake <tiktodz@soulvibe-team.cloud>
2025-06-14 01:26:36 +0700

    X00TD: Update thermal-engine and vendorsetup
    Change-Id: If50280d33958fed1e9938960fd88b0e737f7e8b1

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,7 +3,10 @@
 export TZ=Asia/Jakarta
 
 rm -rf kernel/asus/sdm660
-git clone --depth=1 --recursive https://github.com/Kneba/kernel_asus_sdm660 -b clo-ksun kernel/asus/sdm660
+git clone --single-branch --recursive https://github.com/Kneba/kernel_asus_sdm660 -b uclamp kernel/asus/sdm660
+cd kernel/asus/sdm660
+git revert 079a547168e74a57b7431652554eddef0e901a59 --no-edit
+cd ../../..
 
 # Force Using KSU
 rm -rf system/extras/su
