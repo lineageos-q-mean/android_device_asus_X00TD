d3713b6d7b38c1599d9a915adbe3d2b27111bc0b
texascake <tiktodz@soulvibe-team.cloud>
2026-05-27 15:11:58 +0700

    X00TD: Add EAS powerhint variant to device.mk

--- device.mk
+++ device.mk
@@ -11,6 +11,9 @@ $(call inherit-product, device/asus/sdm660-common/sdm660.mk)
 TARGET_SCREEN_HEIGHT := 2160
 TARGET_SCREEN_WIDTH := 1080
 
+# EAS powerhint
+EAS_POWERHINT_VARIANT := 636
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
